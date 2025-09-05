a90508f165012c3544a292f35c9f68b07d89f5c2
gmaitto <coutogui2007@gmail.com>
2025-09-05 08:58:31 -0300

    opa

--- README.md
+++
@@ -1,1 +0,0 @@
-# maitto.responsividade
--- contato/script.js
+++ contato/script.js
@@ -1,3 +1,25 @@
 function q(){
-    
+    var nome = $('#nome').val()
+    var senha = $('#senha').val()
+
+    if(nome && senha && nome === "admin" && senha === "admin"){
+        const user = {
+            name:nome,
+            dataEntrada: new Date(),
+            id: Math.floor(Math.random() * 100000)
+        };
+
+        localStorage.setItem('usuario', JSON.stringify(user))
+        window.location.href = '..//index.html'
+    }else{
+        document.getElementById('error-modal').style.display = 'flex'
+        document.getElementById('nome').style.borderBottom = '3px solid red'
+        document.getElementById('senha').style.borderBottom = '3px solid red'
+    }
+}
+
+function fecharError(){
+        document.getElementById('error-modal').style.display = 'none'
+        document.getElementById('nome').style.borderBottom = '2px solid #090909'
+        document.getElementById('senha').style.borderBottom = '2px solid #090909'
 }
